13da7851e325dd392df5a4ca241ebc513f88d9f4
SebaVRdev <sebarm177@gmail.com>
2023-05-24 19:37:25 -0400

    first commit

--- server.js
+++ server.js
@@ -6,21 +6,23 @@ const mongoose   = require('mongoose');
 const apiRoutes  = require('./routes');
 
 const app    = express();
+
 const PORT   = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
 
 
 // CONEXIÓN A BASE DE DATOS
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(db => console.log("Conexión a BD correcta"))
+    .then(db => {
+        console.log("Conexión a BD correcta")
+        // SERVIDOR WEB
+        app.listen(PORT, () => console.log("Servidor iniciado..."));
+    })  
     .catch(error => console.log("Error al conectarse a la BD" + error));
 
-
 // MIDDLEWARE
+
 app.use(cors());            // Soporte para CORS
 app.use(express.json());    // IMPORTANTE: Poner esto antes de las rutas
 app.use('/api', apiRoutes);
 app.use(express.static(path.join(__dirname , 'public')));
-
-// SERVIDOR WEB
-app.listen(PORT, () => console.log("Servidor iniciado..."));
